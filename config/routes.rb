9efda68e714b69fc3dd5bbdfdb523aff6208be6d
Dawid <kolodziejdawid02@gmail.com>
2023-04-25 00:42:26 +0200

    Add bootstrap, style devise views, add sample header

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   root "home#index"
 
   devise_for :users
+
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    get "register", to: "devise/registrations#new"
+    delete "logout", to: "devise/sessions#destroy"
+  end
 end
