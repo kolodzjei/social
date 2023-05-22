1633c8f82ec7735f152904ea40e3c9fb8ae72866
Dawid Kolodziej <kolodziejdawid02@gmail.com>
2023-05-23 01:17:09 +0200

    Update specs for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe(Comment, type: :model) do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:post_id) }
   end
+
+  describe "#replies_count" do
+    let(:user) { create(:user) }
+    let(:post) { create(:post, user: user) }
+    let(:comment) { create(:comment, user: user, post: post) }
+    let!(:reply) { create(:reply, user: user, comment: comment) }
+
+    before do
+      create(:reply, user: user, comment: comment, parent_reply: reply)
+    end
+
+    it "returns the number of replies without nested replies" do
+      expect(comment.replies_count).to(eq(1))
+    end
+  end
 end
